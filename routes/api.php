e548ba21650f45225dbd36c26e6f2cfe4cd6bc8c
lecon20122 <lecon20122@gmail.com>
2022-03-21 16:26:01 +0200

    update code base

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use App\Http\User\Controllers\UserController;
 */
 
 //user Routes
-Route::group([],base_path().'app/Http/User/Routes/userRoutes.php');
+Route::group([],'app/Http/User/Routes/userRoutes.php');
 
 //Product Routes
-Route::group([],base_path().'app/Http/User/Routes/productRoutes.php');
+Route::group([],'app/Http/User/Routes/productRoutes.php');

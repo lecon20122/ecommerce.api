d60ff7791458fa73f227b3b598282331052df746
lecon20122 <lecon20122@gmail.com>
2022-03-23 22:10:32 +0200

    StoreProductRequest import

--- app/Domain/Product/Services/ProductService.php
+++ app/Domain/Product/Services/ProductService.php
@@ -3,23 +3,23 @@
 namespace App\Domain\Product\Services;
 
 use App\Domain\Product\Models\Product;
-use App\Http\Product\Requests\storeProductRequest;
+use App\Http\Product\Requests\StoreProductRequest;
 use App\Support\Jobs\UploadImageJob;
 use Illuminate\Support\Str;
 
 class ProductService
 {
     /**
-     * @param storeProductRequest $request
+     * @param StoreProductRequest $request
      * @return mixed
      */
 
-    public function store(storeProductRequest $request)
+    public function store(StoreProductRequest $request)
     {
         $product = Product::create($request->validated());
 
         if ($product && $request->hasFile('images')) {
-//            UploadImageJob::dispatchAfterResponse($product, 'images', 'products');
+            UploadImageJob::dispatchAfterResponse($product, 'images', 'products');
         }
         return $product;
     }

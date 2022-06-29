e17d0390cb83bd7dda090e32219ee8d1e47e246e
lecon20122 <lecon20122@gmail.com>
2022-06-29 08:54:03 +0200

    make exception for products and variation routes(index)

--- app/Http/Product/Routes/api.php
+++ app/Http/Product/Routes/api.php
@@ -4,5 +4,8 @@ use App\Http\Product\Controllers\ProductController;
 use App\Http\Product\Controllers\VariationController;
 use \Illuminate\Support\Facades\Route;
 
-Route::apiResource('products', ProductController::class)->middleware('auth:sanctum');
-Route::apiResource('variations', VariationController::class)->middleware('auth:sanctum');
+Route::apiResource('products', ProductController::class)->middleware('auth:sanctum')->except('index');
+Route::get('products', [ProductController::class, 'index'])->name('products.index');
+
+Route::apiResource('variations', VariationController::class)->middleware('auth:sanctum')->except('index');
+Route::get('variations', [VariationController::class, 'index'])->name('variations.index');

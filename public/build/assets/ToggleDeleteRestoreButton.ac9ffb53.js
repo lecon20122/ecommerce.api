a8dd590006ecc218cce9827dab120f6dc056b4e3
lecon20122 <lecon20122@gmail.com>
2022-09-23 16:48:08 +0200

    removing ag-grid

---
+++ public/build/assets/ToggleDeleteRestoreButton.ac9ffb53.js
@@ -0,0 +1,1 @@
+import{a as t}from"./app.1fdb214b.js";import{B as l}from"./button.034a529d.js";function s({handleOnClickDelete:o,handleOnClickRestore:r,params:e}){return e.deleted_at?t(l,{className:"mr-2",type:"default",onClick:()=>r(e),children:"UNDO"}):t(l,{className:"mr-2",type:"default",onClick:()=>o(e),children:"DELETE"})}export{s as T};
---
+++ public/build/assets/inheritsLoose.ba053e4e.js
@@ -0,0 +1,1 @@
+import{a as e}from"./assertThisInitialized.00356ca2.js";function p(t,o){t.prototype=Object.create(o.prototype),t.prototype.constructor=t,e(t,o)}export{p as _};

6dd22d5f6d3ad06d582148e0541ec30f21a0719c
lecon20122 <lecon20122@gmail.com>
2022-09-24 21:20:14 +0200

    removing ag-grid

--- public/build/assets/AntDesignDataTable.3e9e60f0.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./app.20f328c1.js";import{T as e}from"./Table.03b0f4cf.js";function n({rowKey:r="id",columns:a,dataSource:t}){return o(e,{rowKey:r,columns:a,dataSource:t,scroll:{x:!0}})}export{n as A};

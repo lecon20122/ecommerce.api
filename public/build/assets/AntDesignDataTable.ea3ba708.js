faf9d4d2213ade325e3d9f56bfa38b160a1ec218
lecon20122 <lecon20122@gmail.com>
2022-10-01 14:36:13 +0200

    product browser

--- public/build/assets/AntDesignDataTable.ea3ba708.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./app.07f21977.js";import{T as e}from"./Table.71410d41.js";function n({rowKey:r="id",columns:a,dataSource:t}){return o(e,{rowKey:r,columns:a,dataSource:t,scroll:{x:!0}})}export{n as A};
--- resources/js/components/client/includes/CategoryRectangleComponent.tsx
+++ resources/js/components/client/includes/CategoryRectangleComponent.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {HomePageCategory} from "../../../types/CategoryType";
 import CategoryCircle from "./CategoryCircle";
+import {Inertia} from "@inertiajs/inertia";
+import route from "ziggy-js";
 
 interface Props {
   categories: HomePageCategory[]
@@ -10,9 +12,15 @@ interface Props {
 
 export default function CategoryRectangleComponent({categories, locale}: Props) {
 
+  const onClickHandle = (category: HomePageCategory) => {
+    Inertia.get(route('shop.by.category', category))
+  }
 
   const categoriesItems = categories.map((category) => (
-    <div className='w-[110px] h-[44px] lg:w-[240px] lg:h-[88px] bg-[#F6E4E3] m-1 lg:flex-[1_1_240px] flex-[1_1_28%]'>
+    <div
+      className='w-[110px] h-[44px] lg:w-[240px] lg:h-[88px] bg-[#F6E4E3] m-1 lg:flex-[1_1_240px] flex-[1_1_28%] cursor-pointer'
+      onClick={(e) => onClickHandle(category)}
+    >
       <div className='flex items-center justify-center h-full'>
         <h6 className={'lg:text-2xl uppercase'}>{category.title.en}</h6>
       </div>

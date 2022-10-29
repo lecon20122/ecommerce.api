a3bf71fecf0bccbadd82f4fc85bf1e3b5db1db3e
lecon20122 <lecon20122@gmail.com>
2022-10-29 19:26:18 +0200

    cart 80% finished

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,5 +68,6 @@ class Kernel extends HttpKernel
         'abilities' => \Laravel\Sanctum\Http\Middleware\CheckAbilities::class,
         'ability' => \Laravel\Sanctum\Http\Middleware\CheckForAnyAbility::class,
         'locale' => \App\Http\Middleware\SetLocale::class,
+        'cors' => \App\Http\Middleware\Cors::class
     ];
 }

b301a6360f7923d0daa73c89c41b6cdcc47cc69f
lecon20122 <lecon20122@gmail.com>
2022-12-24 13:51:38 +0200

    working on checkout page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Domain\Location\Enums\LocationEnums;
 use App\Domain\Location\Models\Address;
+use App\Domain\Location\Models\City;
 use App\Domain\Location\Models\District;
+use App\Domain\Location\Models\Governorate;
 use App\Domain\Variation\Models\VariationType;
 use Domain\User\Models\User;
 use Illuminate\Database\Seeder;
@@ -24,10 +26,30 @@ class DatabaseSeeder extends Seeder
 //            StartupSeeder::class,
 //            ProductSeeder::class,
 //        ]);
-        Address::factory()->create([
-            'district_id' => 1,
-            'addressable_id' => 136,
-            'addressable_type' => User::class,
+        $gov = Governorate::factory()->create([
+            'name' => 'Cairo',
+        ]);
+        $city = City::factory()->create([
+            'governorate_id' => $gov->id,
+            'name' => 'Nasr City'
+        ]);
+        $city2 = City::factory()->create([
+            'governorate_id' => $gov->id,
+            'name' => 'Maadi'
+        ]);
+
+        District::factory()->create([
+            'city_id' => $city->id,
+            'district' => 'Al Hay Sabea'
+        ]);
+        District::factory()->create([
+            'city_id' => $city->id,
+            'district' => 'Al Hay Sades'
+        ]);
+
+        District::factory()->create([
+            'city_id' => $city2->id,
+            'district' => '90 Street'
         ]);
     }
 }

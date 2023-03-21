6756895e100b6704d0bce90052af3cd59475a654
lecon20122 <lecon20122@gmail.com>
2023-03-21 15:05:04 +0200

    sell request api

--- app/Http/Store/Routes/api.php
+++ app/Http/Store/Routes/api.php
@@ -6,7 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::name('sell.')->prefix('sell/v1')->middleware(['auth:sanctum'])->group(function () {
-
     Route::post('seller-request', [StoreController::class, 'createSellerRequest'])->name('create.seller.request');
-
 });

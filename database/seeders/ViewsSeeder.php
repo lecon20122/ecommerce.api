258f1ca0409e8b089af5109ff698f53bb62b4991
lecon20122 <lecon20122@gmail.com>
2023-05-10 21:44:02 +0300

    fixing views summary

--- database/seeders/ViewsSeeder.php
+++ database/seeders/ViewsSeeder.php
@@ -21,7 +21,9 @@ class ViewsSeeder extends Seeder
         $yesterday = Carbon::yesterday(config('app-settings.timezone'));
 
         echo "Start Date: {$yesterday->format('Y-m-d H:i:s')}\n";
-
+        echo "timezone :" . config('app-settings.timezone') . "\n";
+        echo "time now" . now()->format('Y-m-d H:i:s') . "\n";
+        
         foreach ($approvedActiveProducts as $product) {
 
             View::factory(rand(100, 500))->create([
